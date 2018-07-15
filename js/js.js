34aeb0a226d0049a0f94e960e1c1d12fbc00ae65
Avokado <alotofcode95@gmail.com>
2018-07-16 04:17:29 +0600

    success ^_^ But is the result correct?

--- js/js.js
+++ js/js.js
@@ -1,32 +1,30 @@
 /**
  * Created by hz on 15.07.2018.
  */
+var HVLtoRGB=function HVLtoRGB (h,c,x,m) {
+    var r =[];
+    if(h>=0 && h<60) r.push(c,x,0);
+    if(h>=60 && h<120) r.push(x,c,0);
+    if(h>=120 && h<180) r.push(0,c,x);
+    if(h>=180 && h<240) r.push(0,x,c);
+    if(h>=240 && h<300) r.push(x,0,c);
+    if(h>=300 && h<360) r.push(c,0,x);
+    for (var i=0;i<r.length;i++){
+        r[i]=Math.round((r[i]+m)*255);
+    }
+    return r;
+};
 $('#btn').click(function () {
     var h=$('#h').val();
     var s=$('#s').val();
     var l=$('#l').val();
-
-    var Q = l<0.5 ? l*(1+s) : l+s-(l*s);
-    var P = 2*l-Q;
-    var Hk = h/360;
-    var T = [Hk+1/3,Hk, Hk-1/3];
-    for (var i=0; i<T.length; i++) {
-        if (T[i] < 0)
-            ++T[i];
-        if (T[i] > 1)
-            --T[i];
-    }
-
-    for (var j=0; j<T.length; j++){
-        if (T[j]<1/6)
-            T[j]=P+(Q-P)*6*T[j];
-        else if (T[j]>=1/6 && T[j]<0.5)
-            T[j]=Q;
-        else if(T[j]>=0.5 && T[j]<2/3)
-            T[j]=P+((Q-P)*(2/3-T[j])*6);
-        else
-            T[j]=P;
-    }
-    var colorString = T[0]+","+T[1]+","+T[2];
-    $('#result').val(colorString);
+    var Hk=h/60;
+    var c=(1-Math.abs(2*l))*s;
+    var x=c*(1-Math.abs(Hk%2-1));
+    var m=l-c/2;
+    var rgb= HVLtoRGB(h,c,x,m);
+    var colorString = rgb[0]+","+rgb[1]+","+rgb[2];
+    var textInp=$('#result');
+    textInp.val(colorString);
+    textInp.css('background-color','rgb('+colorString+')');
 });
